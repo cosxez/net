29967dce355bfdc4c18c34df309ef5ed0599b9d0
cosxez <faceless113x@gmail.com>
2026-03-28 22:21:50 +0400

    Update client.cpp

--- hkm-client/client.cpp
+++ hkm-client/client.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <cstring>
 #include <string>
+#include <thread>
+#include <chrono>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -42,12 +44,7 @@ int main()
 				while (ip_addr.empty()){std::cout<<"Enter ip address: ";getline(std::cin,ip_addr);}
 				std::string port;
 				while (port.empty()){std::cout<<"Enter port: ";getline(std::cin,port);}
-				std::string url_s;
-				if (is_ar==true)
-				{
-					while (url_s.empty()){std::cout<<"Enter url: ";getline(std::cin,url_s);}
-				}
-
+				
 				memset(&addr,0,sizeof(addr));
 				try
 				{
@@ -55,65 +52,49 @@ int main()
 					addr.sin_port=htons(std::stoi(port));
 					inet_pton(AF_INET,ip_addr.c_str(),&addr.sin_addr);
 					connect(sock,(struct sockaddr*)&addr,sizeof(addr));
-					
-					if (is_ar==true && !url_s.empty())
+					char buffer[2048];
+
+					if (is_ar==true)
 					{
-						send(sock,url_s.c_str(),url_s.size(),0);
-						char buffer[1024];
-						size_t stp=recv(sock,buffer,sizeof(buffer),0);
-						buffer[stp]='\0';
-						if (stp==1 && buffer[0]=='1')
+						std::string fhead="t.n.c";
+						send(sock,fhead.c_str(),fhead.size(),0);
+						int sb=recv(sock,buffer,sizeof(buffer)-1,0);
+						buffer[sb]='\0';
+						for (int i=0;i<sb;i++){std::cout<<buffer[i];}
+						
+						while (true)
 						{
-							size_t flss;
-							stp=recv(sock,&flss,sizeof(flss),0);
-							std::vector<std::string> fls;
-
-							stp=0;
-							while (stp<flss)
+							std::string tcmd;
+							while (tcmd.empty()){std::cout<<"ar-t-terminal>";getline(std::cin,tcmd);}
+							if (tcmd=="exit"){close(sock);break;}
+							if (tcmd.size()>4 && tcmd[0]=='p' && tcmd[1]=='u' && tcmd[2]=='s' && tcmd[3]=='h')
 							{
-								size_t crtp=recv(sock,buffer,sizeof(buffer),0);
-								stp+=crtp;
-								buffer[crtp]='\0';
-								std::string lspw;
-								for (int j=0;j<crtp;j++){lspw+=buffer[j];}
-								fls.push_back(lspw);
-							}
-							for (auto &c:fls){std::cout<<c<<std::endl;}
-							
-
-							std::string fln;
-							while (true)
-							{
-								std::cout<<"Enter filename(if you want download, \"exit\" - for close connect): ";
-								getline(std::cin,fln);
-								
-								if (fln=="exit"){std::string cl_conn="close";send(sock,cl_conn.c_str(),cl_conn.size(),0);break;}
-								if (!fln.empty())
+								std::string filename;
+								for (int i=5;tcmd[i]!=' ';i++){filename+=tcmd[i];}
+								std::ifstream file(filename);
+								if (file.is_open())
 								{
-									bool file_ex=false;
-									for (int i=0;i<fls.size();i++){if (fln==fls[i]){file_ex=true;}}
-									if (file_ex==true)
-									{
-										std::string endline="GET DATA:"+fln;
-										send(sock,endline.c_str(),endline.size(),0);
-										size_t fs;
-										recv(sock,&fs,sizeof(fs),0);
-										std::vector<char> fs_data(fs);
-										size_t fsds=0;
-										while (fsds<fs_data.size())
-										{
-											fsds+=recv(sock,fs_data.data()+fsds,fs_data.size(),0);
-										}
-	
-										std::ofstream file(fln,std::ios::binary);
-										file.write(reinterpret_cast<const char*>(fs_data.data()),fs_data.size());
-										file.close();
-									}
+									file.seekg(0,std::ios::end);
+									size_t fs=file.tellg();
+									file.seekg(0,std::ios::beg);
+									
+									std::vector<char> fld(fs);
+									file.read(reinterpret_cast<char*>(fld.data()),fld.size());
+									file.close();
+									
+									send(sock,tcmd.c_str(),tcmd.size(),0);
+									std::this_thread::sleep_for(std::chrono::milliseconds(100));
+									send(sock,&fs,sizeof(fs),0);
+									std::this_thread::sleep_for(std::chrono::milliseconds(100));
+									send(sock,fld.data(),fld.size(),0);
+									
+									sb=recv(sock,buffer,sizeof(buffer)-1,0);
+									buffer[sb]='\0';
+									for (int i=0;i<sb;i++){std::cout<<buffer[i];}
 								}
-								else{std::string cl_conn="close";send(sock,cl_conn.c_str(),cl_conn.size(),0);}
+								else{std::cout<<"file dont open\n";}
 							}
 						}
-						if (stp==1 && buffer[0]=='0'){std::cout<<"URL error: site doesnt exist\n";}
 					}
 					if (is_ar==false)
 					{
@@ -126,7 +107,7 @@ int main()
 						}
 					}
 				}
-				catch(std::exception &e){std::cout<<"Error: "<<e.what()<<std::endl;}
+				catch(std::exception &e){std::cout<<"Error: "<<e.what()<<std::endl;close(sock);}
 			}
 			close(sock);
 		}
